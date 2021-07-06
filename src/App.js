9b0fd73db3d235b4c91777901fa17975fd8303a2
Renata Nunes <renatanunes231@yahoo.com.br>
2021-07-06 04:32:16 -0300

    Exercicios do 1 ao 13 resolvidos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import StrictAccess from './components/StrictAccess';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users">Users</Link></li>
+          <li><Link to="/strictaccess">StrictAccess</Link></li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route
+          path="/users/:id"
+          render={ (props) => <Users {...props} greetingsMessage="Good Morning" /> }
+        />
+        <Route
+          path="/strictaccess"
+          render={ () => <StrictAccess user={ { username: 'joao', password: '1234' } } /> }
+        />
+        <Route path="/about" component={ About } />
+        <Route path="/" component={ Home } />
+      </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
-    return (
-      <div>
-        <h2>Users</h2>
-        <p> My awesome Users component </p>
-      </div>
-    );
+    const { greetingsMessage } = this.props;
+    // id terá o valor passado pela url localhost:3000/users/id_qualquer
+    const { id} = this.props.match.params;
+      return (
+        <div>
+          <h2>Users</h2>
+          <p> { `${greetingsMessage} user ${id}` }, My awesome Users component </p>
+        </div>
+      )
   }
 };
 
